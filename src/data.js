963375a5ad24d5141b3225d80777001d84c7ad6f
strange086 <akashachu0000@gmail.com>
2025-04-26 21:39:59 +0530

    Initial commit - Portfolio ready

---
+++ src/data.js
@@ -0,0 +1,75 @@
+export const projects = [
+    {
+      title: "Eco-Drive",
+      subtitle: "Python + ML",
+      description:
+        "An Ev Routing App that uses Reinforcement Learning to suggest the best route for electric vehicles, considering factors like battery life and charging stations.",
+      image: "Eco-Drive.png",
+      link: "https://www.linkedin.com/posts/akash-s-l-290085255_ecodrive-electricvehicles-sustainability-activity-7317049632319066112-udkm?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD7WO0wByhunZHys-00Iv5by6Usu8xTUK5k",
+    },
+    {
+      title: "Wikipedia Redesign",
+      subtitle: "HTML/CSS + Responsive UI",
+      description:
+        "A modern, mobile-first redesign of Wikipedia’s homepage using clean HTML and CSS to improve UX and visual appeal.",
+      image: "wikipedia.jpg",
+      link: "https://github.com/yourusername/wikipedia_redesigned",
+    },
+    {
+        title: "Foodie-Meta",
+        subtitle: "HTML/CSS + Responsive UI",
+        description:
+          "A reminder app that helps users track their food intake and set reminders for meals. Built with HTML, CSS, and JavaScript.",
+        image: "recipe.jpg",
+        link: "https://www.linkedin.com/posts/akash-s-l-290085255_foodie-meta-a-smarter-way-to-manage-activity-7317401500173979648-yoN6?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD7WO0wByhunZHys-00Iv5by6Usu8xTUK5k",
+      },
+    {
+      title: "Tic-Tac-Toe",
+      subtitle: "JavaScript + DOM",
+      description:
+        "A classic Tic-Tac-Toe game implemented in vanilla JavaScript with win tracking and responsive gameplay.",
+      image: "tictactoe.png",
+      link: "https://github.com/yourusername/tictactoe",
+    },
+    {
+      title: "To-Do List",
+      subtitle: "React + LocalStorage",
+      description:
+        "A simple to-do list application built in React with persistent storage using the browser's localStorage API.",
+      image: "todo.png",
+      link: "https://github.com/yourusername/todo-list",
+    },
+  ];
+  
+  
+  export const testimonials = [
+    {
+      quote:
+        "Great developer. He was able to take my ideas and turn them into a reality.",
+      image: "https://randomuser.me/api/portraits/men/1.jpg",
+      name: "Jesse Hicks",
+      company: "Zoozle",
+    },
+    {
+      quote:
+        "Good fellow. Fast and efficient. I would recommend him to anyone looking for a developer.",
+      image: "https://randomuser.me/api/portraits/men/94.jpg",
+      name: "Ruben Alvarez",
+      company: "Dooble.io",
+    },
+  ];
+  
+  export const skills = [
+    "JavaScript",
+    "React",
+    "HTML",
+    "CSS",
+    "Node.js",
+    "Firebase",
+    "Python",
+    "Machine Learning",
+    "C++",
+    "Java",
+    "C#",
+    "NLP"
+  ];
